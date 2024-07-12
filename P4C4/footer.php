3223d2c3a1e2493c83c15cfb76a1441195275bc0
Angele-Bzl <169182839+Angele-Bzl@users.noreply.github.com>
2024-07-12 16:35:28 +0200

    Add files via upload

---
+++ P4C4/footer.php
@@ -0,0 +1,8 @@
+<!-- FOOTER PRESENT EN BAS DES PAGES DU SITES -->
+<footer class="bg-light text-center text-lg-start mt-auto">
+    <div class="text-center p-3">
+        <p>© 2024 Copyright</p>
+        <p>projet d'Angèle Barzilai</p>
+        <p>avec l'assistance d'<a class="text-dark" href="https://openclassrooms.com/">OpenClassrooms</a> et de <a class="text-dark" href='https://2iacademy.com/'>2i Academy</a></p>
+    </div>
+</footer>
---
+++ P4C4/recipes_post_update.php
@@ -0,0 +1,109 @@
+<!-- PROCESSUS DE MODIFICATION DE LA RECETTE, PUIS AFFICHAGE D'UN MESSAGE DE VALIDATION -->
+<?php
+session_start();
+
+require_once(__DIR__ . '/isConnect.php');
+require_once(__DIR__ . '/config/mysql.php');
+require_once(__DIR__ . '/databaseconnect.php');
+
+/**
+ * On ne traite pas les super globales provenant de l'utilisateur directement,
+ * ces données doivent être testées et vérifiées.
+ */
+$postData = $_POST;
+
+if ( //vérification que le formulaire est complet
+    !isset($postData['id'])
+    || !is_numeric($postData['id'])
+    || empty($postData['title'])
+    || empty($postData['recipe'])
+    || trim(strip_tags($postData['title'])) === ''
+    || trim(strip_tags($postData['recipe'])) === ''
+) {
+    echo 'Il manque des informations pour permettre l\'édition du formulaire.';
+    return;
+}
+
+//récupération des données
+$id = (int)$postData['id'];
+$title = trim(strip_tags($postData['title']));
+$recipe = trim(strip_tags($postData['recipe']));
+$picture = $_FILES['picture'];
+
+//insérer les données dans la BDD
+$insertRecipeStatement = $mysqlClient->prepare('UPDATE recipes SET title = :title, recipe = :recipe WHERE recipe_id = :id');
+$insertRecipeStatement->execute([
+    'title' => $title,
+    'recipe' => $recipe,
+    'id' => $id,
+]);
+
+
+//procédure si une image est téléchargée
+$isFileLoaded = false;
+// Testons si le fichier a bien été envoyé et s'il n'y a pas des erreurs
+if (isset($_FILES['picture']) && $_FILES['picture']['error'] === 0) {
+    // Testons, si le fichier est trop volumineux
+    if ($_FILES['picture']['size'] > 1000000) {
+        echo "L'envoi n'a pas pu être effectué, erreur ou image trop volumineuse";
+        return;
+    }
+
+    // Testons, si l'extension n'est pas autorisée
+    $fileInfo = pathinfo($_FILES['picture']['name']);
+    $extension = $fileInfo['extension'];
+    $allowedExtensions = ['jpg', 'jpeg', 'gif', 'png'];
+    if (!in_array($extension, $allowedExtensions)) {
+        echo "L'envoi n'a pas pu être effectué, l'extension {$extension} n'est pas autorisée";
+        return;
+    }
+
+    // Testons, si le dossier uploads est manquant
+    $path = __DIR__ . '/uploads/';
+    if (!is_dir($path)) {
+        echo "L'envoi n'a pas pu être effectué, le dossier uploads est manquant";
+        return;
+    }
+
+    // On peut valider le fichier et le stocker définitivement
+    $picture1['name']= $id.'.jpg'; //changement du nom de l'image dans les uploads
+        
+    move_uploaded_file($_FILES['picture']['tmp_name'], $path . basename($picture1['name']));
+    $isFileLoaded = true;
+} else {
+    echo 'ELSE';
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site de Recettes - Création de recette</title>
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+    >
+</head>
+<body class="d-flex flex-column min-vh-100">
+    <div class="container">
+
+        <?php require_once(__DIR__ . '/header.php'); ?>
+        <h1>Recette modifiée avec succès !</h1>
+
+        <div class="card">
+
+            <div class="card-body">
+                <h5 class="card-title"><?php echo($title); ?></h5>
+                <p class="card-text"><b>Email</b> : <?php echo $_SESSION['LOGGED_USER']['email']; ?></p>
+                <p class="card-text"><b>Recette</b> : <?php echo $recipe; ?></p>
+                <p class="card-text"><b>Image</b> : <?php echo $picture['name']; ?></p>
+            </div>
+        </div>
+    </div>
+    <?php require_once(__DIR__ . '/footer.php'); ?>
+</body>
+</html>
